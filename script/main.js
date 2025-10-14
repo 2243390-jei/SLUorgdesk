dccb6a348225797c3613c43f415aaf0438b40e46
Jei <2243390@slu.edu.ph>
2025-10-14 12:46:29 +0800

    added functionality for authentication and implemented SSO

---
+++ script/main.js
@@ -0,0 +1,53 @@
+// --- Dummy users  ---
+const users = [
+  { role: "student", email: "student@slu.edu.ph", password: "student123" },
+  { role: "osas",    email: "osas@slu.edu.ph",    password: "osas123" },
+  { role: "admin",   email: "admin@slu.edu.ph",   password: "admin123" }
+];
+
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  const user = users.find(u => u.email === email && u.password === password);
+  if (user) {
+    alert(`Login successful! Welcome, ${user.role.toUpperCase()}.`);
+
+    // Redirect based on user role and folder path so ito na yung home para sa mga users
+    switch (user.role) {
+      case "student":
+        window.location.href = "student/home.html";
+        break;
+      case "osas":
+        window.location.href = "osas/dashboard.html";
+        break;
+      case "admin":
+        window.location.href = "admin/dashboard.html";
+        break;
+    }
+  } else {
+    alert("Invalid email or password. Please try again.");
+  }
+});
+// --- Google Identity Services (SSO) ---
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: "687307417693-f5v0ljfh76qpgcrk6uf70io0pe4me6d0.apps.googleusercontent.com",
+    callback: handleCredentialResponse
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("g_id_signin"),
+    { theme: "outline", size: "large", text: "signin_with" }
+  );
+};
+
+function handleCredentialResponse(response) {
+  const data = JSON.parse(atob(response.credential.split('.')[1]));
+  console.log("Google User Data:", data);
+
+  alert(`Logged in as ${data.email}`);
+
+
+  window.location.href = "student/home.html";
+}
