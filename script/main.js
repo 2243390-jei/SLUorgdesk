b988e6d39ea6e69bb131c4dfaeec705cc72fe18a
boac <boacargao@gmail.com>
2025-10-23 15:29:16 +0800

    Added the initial database connection in the OSASorgs files to the mongoDB database

--- script/main.js
+++ script/main.js
@@ -118,46 +118,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  // OSAS Table Logic
-  const organizations = [
-    { 
-      name: "ICON", 
-      school: "SAMCIS", 
-      programs: "BSIT, BSCS, BMMA", 
-      image: "/Images/orgs/ICON.jpg" 
-    },
-    { 
-      name: "JPIA", 
-      school: "SAMCIS", 
-      programs: "BSBA, BSAC", 
-      image: "/Images/orgs/JPIA.jpg" 
-    },
-    { 
-      name: "RPG", 
-      school: "SAMCIS", 
-      programs: "SAMCIS", 
-      image: "/Images/orgs/RPG.jpg" 
-    }
-  ];
 
-  const tableBody = document.getElementById("orgTableBody");
-
-  if (tableBody) {
-    tableBody.innerHTML = "";
-
-    organizations.forEach(org => {
-      const row = document.createElement("tr");
-
-      row.innerHTML = `
-        <td><img src="${org.image}" alt="${org.name} logo" class="org-logo"></td>
-        <td>${org.name}</td>
-        <td>${org.school}</td>
-        <td>${org.programs}</td>
-      `;
-
-      tableBody.appendChild(row);
-    });
-  }
 });
 
 
