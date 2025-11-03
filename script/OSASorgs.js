ee4cb6b02d6b6789dcfa3749efd253b8c0c78331
boac <boacargao@gmail.com>
2025-11-03 19:27:01 +0800

    Fixed the naming

--- script/OSASorgs.js
+++ script/OSASorgs.js
@@ -7,7 +7,7 @@ let organizationsData = [];
 
 async function loadOrganizations() {
   try {
-    const response = await fetch("../dataFetch/fetchOrganizations.php");
+    const response = await fetch("../dataFetch/fetchDatabase.php");
     const organizations = await response.json();
     organizationsData = organizations; // save for filtering
 
