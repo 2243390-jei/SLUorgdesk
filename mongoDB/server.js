a2d03c6759d79fbd72721c683bded3e1dd6517be
boac <boacargao@gmail.com>
2025-11-03 12:09:45 +0800

    Small changes

--- mongoDB/server.js
+++ mongoDB/server.js
@@ -119,5 +119,5 @@ app.get("/api/Submission/:submissionId", async (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
