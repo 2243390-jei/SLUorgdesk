463c577061af9a16d91807a38e85ee92f71260b4
JARED EIZEN B. FACUN <eizen453@gmail.com>
2025-12-10 21:29:49 +0800

    add the nav in act logs

--- public/admin/orgmanagement.php
+++ public/admin/orgmanagement.php
@@ -34,6 +34,7 @@ if (empty($_SESSION['logged_in'])) {
         <li><a href="dashboard.php">Dashboard</a></li>
         <li class="active"><a href="orgmanagement.php">Org Management</a></li>
         <li><a href="usermanagement.php">User Management</a></li>
+        <li><a href="activitylogs.php">Activity Logs</a></li>        
       </ul>
     </nav>
   </aside>
--- public/admin/usermanagement.php
+++ public/admin/usermanagement.php
@@ -34,6 +34,7 @@ if (empty($_SESSION['logged_in'])) {
         <li><a href="dashboard.php">Dashboard</a></li>
         <li><a href="orgmanagement.php">Org Management</a></li>
         <li class="active"><a href="usermanagement.php">User Management</a></li>
+        <li><a href="activitylogs.php">Activity Logs</a></li>
       </ul>
     </nav>
   </aside>
