3cacddc317626b59fcc3a2a7a4a090be9c377e47
Jei <2243390@slu.edu.ph>
2025-12-11 21:28:28 +0800

    semi fixed

--- public/osas/orgs.php
+++ public/osas/orgs.php
@@ -10,6 +10,7 @@ AuthMiddleware::requireRole('OSAS');
     <link rel="stylesheet" href="styles/osasmain.css">
     <link rel="stylesheet" href="styles/OSASorgs.css">
     <link rel="icon" type="image/png" href="../images/Icon.png" sizes="32x32">
+    <script src="script/profile.js" defer></script>
     <script src="script/OSASorgs.js" defer></script>
     <title>Organizations in SLU</title>
 </head>
@@ -34,10 +35,10 @@ AuthMiddleware::requireRole('OSAS');
     <aside class="sidebar" aria-label="Main navigation">
       <div class="sidebar-top">
         <div class="sidebar-profile">
-          <div class="avatar">O</div>
+          <img id="profile-icon" src="../images/user.png" alt="Profile Icon" width="32" height="32">
           <div class="profile-text">
-            <div class="admin-title">OSAS</div>
-            <div class="admin-sub" id="desktopPageTitle">Organizations Dashboard</div>
+            <div class="admin-name">OSAS</div>
+            <div class="admin-email">Loading...</div>
           </div>
         </div>
       </div>
