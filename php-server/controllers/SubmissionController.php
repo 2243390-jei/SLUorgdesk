7a0747a9295defee6c4985517d13d5b70d8201a9
Jecquar Aguilan <aguilanjecquar@gmail.com>
2025-12-11 23:17:10 +0800

    smalls

--- php-server/controllers/SubmissionController.php
+++ php-server/controllers/SubmissionController.php
@@ -10,7 +10,7 @@ class SubmissionController {
     }
 
     public function getAll() {
-        // Get pagination params with defaults
+        // Get params with defaults
         $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 50;
         $offset = isset($_GET['offset']) ? (int)$_GET['offset'] : 0;
         // Fetch all submissions
