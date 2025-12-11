50c12672a46780beb9a3994f8186fc090333bf04
Piamonte Malech <malechpiamonte@gmail.com>
2025-12-11 23:05:28 +0800

    Pushed mobile view for admin

--- public/admin/activitylogs.php
+++ public/admin/activitylogs.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Activity Logs</title>
     <link rel="stylesheet" href="styles/adminmain.css">
     <link rel="stylesheet" href="styles/activitylogs.css">
